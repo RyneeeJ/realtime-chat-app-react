8f69a71a86b2005e105f9c2c79200ceca55b9e72
RyneeeJ <imrynegandia@gmail.com>
2024-11-20 03:21:20 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Realtime chat app practice project
+
+### Made with React + Vite and Supabase
---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div>
+      <h1>HELLO VITE</h1>
+    </div>
+  );
+}
+
+export default App;
