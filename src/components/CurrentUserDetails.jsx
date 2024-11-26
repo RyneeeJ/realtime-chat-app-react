c5320dce0f215b6fb2ecfeb2d6b11c2d378723d1
RyneeeJ <imrynegandia@gmail.com>
2024-11-26 22:38:59 +0800

    Fetched and displayed current user data in the sidebar

--- src/components/CurrentUserDetails.jsx
+++ src/components/CurrentUserDetails.jsx
@@ -1,10 +1,18 @@
+import { useUser } from "../hooks/useUser";
+
 function CurrentUserDetails() {
+  const { user } = useUser();
+  const {
+    email,
+    user_metadata: { avatar_url: image },
+  } = user;
+
   return (
     <div className="flex items-center gap-3 p-4">
       <div className="aspect-square w-14 overflow-hidden rounded-full">
-        <img src="/default-image.jpg" className="object-cover" />
+        <img src={image || "/default-image.jpg"} className="object-cover" />
       </div>
-      <div className="font-semibold">imrynegandia@gmail.com</div>
+      <div className="font-semibold">{email}</div>
     </div>
   );
 }
